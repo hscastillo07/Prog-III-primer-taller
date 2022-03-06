440f34c3133939f7975e8bae3d8aa3d713b8047f
Harold Castillo <hscastillo07@gmail.com>
2022-03-06 18:00:08 -0500

    ejercicio 1 terminado

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace promedio
+{
+    class Program
+    {
+        static void metodo()
+        {
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese la primera nota:");
+            float nota1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la segunda nota:");
+            float nota2 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la tercera nota:");
+            float nota3 = float.Parse(Console.ReadLine());
+            float prom = (nota1 + nota2 + nota3)/3;
+           
+            Console.WriteLine("Su promedio es: "+ prom);
+            
+        }
+    }
+}
